fb7286c63330ff15852ff438936aec07e0e51e28
Sourav De <srvz39@gmail.com>
2017-05-11 18:37:04 +0530

    added pandas

--- navTest.py
+++ navTest.py
@@ -4,7 +4,7 @@ import os
 
 import csv
 
-url="http://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?mf=22&frmdt=01-Apr-2017&todt=30-Apr-2017"
+url="http://portal.amfiindia.com/DownloadNAVHistoryReport_Po.aspx?mf=22&frmdt=01-Apr-2015&todt=30-Apr-2017"
 
 driver = webdriver.PhantomJS(executable_path='C:/Users/608619925/Downloads/phantomjs-2.1.1-windows/phantomjs-2.1.1-windows/bin/phantomjs.exe') # or add to your PATH
 #driver.set_window_size(1024, 768) # optional
@@ -16,11 +16,24 @@ data=preTextEl.text.split(";")
 #sbtn = driver.find_element_by_css_selector('button.gbqfba')
 #sbtn.click()
 #csvFile = open('example.csv', 'w', newline='')
-with open('some.csv', 'w',newline="\n",encoding="utf-8") as f:
-    writer = csv.writer(f,delimiter=";")
+op_file="some.csv"
+with open(op_file, 'w',newline='\n',encoding="utf-8") as f:
+    writer = csv.writer(f,delimiter=';')
     writer.writerows([data])
-    for r in tqdm(range(os.path.getsize("some.csv"))):
+    for r in tqdm(range(os.path.getsize(op_file))):
         pass
+
+my_file_name = "some.csv"
+cleaned_file = "cleansome.csv"
+remove_words = [";"]
+
+with open(my_file_name, 'r', newline='') as infile, open(cleaned_file, 'w',newline='') as outfile:
+    writer = csv.writer(outfile)
+    for line in csv.reader(infile, delimiter=';'),tqdm(range(len(line))):
+        if not any(remove_word in line for remove_word in remove_words):
+            writer.writerow(line)
+    
+    
     
     
 #writer.close
